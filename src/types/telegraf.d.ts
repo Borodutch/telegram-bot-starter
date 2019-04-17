e949e84aae2c4c8c7f20bc318269f163b885463b
Nikita Kolmogorov <backmeupplz@gmail.com>
2019-04-16 20:48:56 -0700

    Update telegraf.d.ts

--- src/types/telegraf.d.ts
+++ src/types/telegraf.d.ts
@@ -2,10 +2,11 @@
 import I18N from 'telegraf-i18n'
 import * as tt from 'telegraf/typings/telegram-types.d'
 import { User } from '../models'
+import { InstanceType } from 'typegoose'
 
 declare module 'telegraf' {
   export class ContextMessageUpdate {
-    dbuser: User
+    dbuser: InstanceType<User>
     i18n: I18N
   }
 
