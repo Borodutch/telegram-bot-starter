5ebc14d7803db830b89e99bc78ed78341d4c1388
Nikita Kolmogorov <backmeupplz@gmail.com>
2021-12-07 11:17:42 -0800

    fix bot constructor

--- src/helpers/bot.ts
+++ src/helpers/bot.ts
@@ -2,6 +2,8 @@ import { Bot } from 'grammy'
 import Context from '@/models/Context'
 import env from '@/helpers/env'
 
-const bot = new Bot<Context>(env.TOKEN)
+const bot = new Bot<Context>(env.TOKEN, {
+  ContextConstructor: Context,
+})
 
 export default bot
